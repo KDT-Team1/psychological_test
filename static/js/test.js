7b591e714221bfeed132d60accf89b7d1c321581
MYUNGGWAN LEE <lmk960201@gmail.com>
2022-10-17 10:04:22 +0900

    FIXED

--- static/js/test.js
+++ static/js/test.js
@@ -10,8 +10,8 @@ function syncReadFile(filename) {
     const contents = readFileSync(filename, 'utf-8');
 
     const arr = contents.split(/\r?\n/);
-)
-return arr;
+
+    return arr;
 }
 
 // --------------------------------------------------------------
