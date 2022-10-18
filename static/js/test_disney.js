7a719653793977b338d3edf71cf0a3a0740f54c6
2hyeseung <leehs226@naver.com>
2022-10-18 15:30:36 +0900

    ADD: test_disney

--- static/js/test_disney.js
+++ static/js/test_disney.js
@@ -1,40 +1,33 @@
-let testQuestions = ['Q1. 해야할 일이 많을 때?',
-    'Q2. 친구의 고민을 들어줄 때?',
-    'Q3. 카페에 갔는데 얼굴만 아는 지인이 있을 때?',
-    'Q4. 가장 중요한 것은?',
-    'Q5. 힘들어 죽겠는데 친한 친구들과 약속이 있을 때?',
-    'Q6. 말도 안되는 이유로 비난을 받았을 때?',
-    'Q7. 언젠가 드라마같은 일이 내 인생에서 일어날 것이다.',
-    'Q8. 아는 사람이 적은 파티에 가야할 때?'
-]
+const testQuestions=[
+'Q1. 디즈니 캐릭터가 된 나는',
+'Q2. 디즈니 캐릭터가 됐는데 뭘하지?',
+'Q3. 디즈니 캐릭터 세상에 있자니',
+'Q4. 오늘 모험의 루트는?',
+'Q5. 모험을 하다가 민가에 쌓아놓은 물건들을 쏟아버렸다',
+'Q6. 옆동네 공주들과 시비가 붙었다',
+'Q7. 갑자기 비가 내리기 시작했다',
+'Q8. 너무 돌아다녔나.. 배가 고프다',
+'Q9. 어느날 눈을 떠보니 디즈니 캐릭터 중 하나로 변하게 되었다!!',
+'Q10. 이제 현실세계로 돌아갈 시간'
+];
+const testAnswers=[
+['대장을 따르는 친구 캐릭터','대장 캐릭터'],
+['대충 주변 상황 파악하고 뒹굴뒹굴~','나만의 왕국을 건설하러 떠난다!'],
+['피곤하다..   그림같은 경치에서 휴식','멋진게 너무 많다!!  여기저기 돌아다닌다'],
+['만나고 싶었던 다른 캐릭터를 만나고 같이 모험을 떠나야지!','아무데나 발길 닿는 대로~'],
+['죄송합니다ᅲᅲ 조심조심 다시 쌓아놓는다','사고쳤지만 모르는 척 자리를 뜬다'],
+['싸우지 마 말리다가 도망간다','너 지금 뭐라고했냐? 싸운다'],
+['나무 밑에서 내리는 비소리를 들으며 감성에 젖는다','공짜 샤워다!!    다른 캐릭터들과 다같이 논다'],
+['내가 먹고싶은 메뉴를 파는 식당을 찾으러 간다','사냥을 시작한다'],
+['좀 쉬자!  냅다 눕는다','그래도 어떻게 땅바닥에 누워,,   묵을 곳을 찾아본다'],
+['공주도 힘드네..    원래 내 인생으로 돌려보내줘~!','괜찮다면 이대로 살고싶다'],
+];
 
-let testAnswers = [
-    [
-        '계획을 세워서 최대한 빨리 처리한다.',
-        '미루고 미루다 마감일 하루 전에 처리한다.',
-        '주변 사람의 도움을 받는다.'
-    ],
-    ['해결책을 제시해준다.', '일단 공감해준다.', '비슷한 나의 경험을 들려준다.'],
-    ['못본 척 한다.', '눈인사만 한다.', '다가가서 아는 척을 한다.'],
-    ['사랑과 우정', '자연과 동물', '돈'],
-    [
-        '사과하고 약속을 미룬 다음 집에서 쉰다.',
-        '얼굴만 비추고 집에 돌아온다.',
-        '친구들을 만나서 신나게 놀며 스트레스를 푼다.'
-    ],
-    ['속으로 삭힌다.', '내 잘못이 아닌걸 차근차근 설명한다.', '화부터 내고 시작한다.'],
-    ['글쎄.. 있을 수도 있고.. 없을 수도 있고.. 반반', '놉! 현실과 드라마는 다르다.', '당연!'],
-    [
-        '아는 사람들이랑만 어울린다.',
-        '참석하지 않거나 얼굴만 비추고 집으로 돌아온다.',
-        '파티에서 새로운 친구를 사귄다.'
-    ]
-]
-for (let i = 0; i < 8; i++) {
+
+for (let i = 0; i < 10; i++) {
     document.getElementById(`q${i}`).innerHTML = `${testQuestions[i]}`;
     document.getElementById(`answer${i}0`).innerHTML = `${testAnswers[i][0]}`;
     document.getElementById(`answer${i}1`).innerHTML = `${testAnswers[i][1]}`;
-    document.getElementById(`answer${i}2`).innerHTML = `${testAnswers[i][2]}`;
 }
 
 document.querySelector('.question0').classList.toggle('question-none');
@@ -49,10 +42,15 @@ progress(percent, page);
 
 for (var i = 0; i < answer.length; i++) {
     answer[i].addEventListener('click', function() {
-        result = result + 1 + parseInt(this.id[7]);
+        if(this.id[7]==0){
+            result-=1;
+        }else{
+            result+=1;
+        }
+        console.log(result);
 
-        for (let j = 0; j < 8; j++) {
-            if (j == 7) {
+        for (let j = 0; j < 10; j++) {
+            if (j == 9) {
                 document.querySelector('.question' + j).classList.toggle(`question-none`);
                 localStorage.setItem('result', `${result}`);
                 // console.log(result);
@@ -60,9 +58,9 @@ for (var i = 0; i < answer.length; i++) {
             } else if (!($('.question' + j).hasClass('question-none'))) {
                 document.querySelector('.question' + j).classList.toggle(`question-none`);
                 document.querySelector('.question' + (j + 1)).classList.toggle(`question-none`);
-                j = 8;
+                j = 10;
                 // progress bar 연동 변수들
-                percent = percent + 12.5;
+                percent = percent + 10;
                 page = page + 1;
                 progress(percent, page);
             }
