b3da116d05006bb9767cd2a20a813c50802ecaf9
MYUNGGWAN LEE <lmk960201@gmail.com>
2022-10-24 11:08:11 +0900

    FIXED: slider

---
+++ static/js/swiper.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2.5,
+    spaceBetween: 20,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
